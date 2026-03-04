a2e579a628483660e81a6d6f379edfb078269d69
aaron <aaron>
2026-03-04 16:40:48 -0500

    Labeled functions to be task or side effect

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ Author: Luke Atkins
 Starter Code: No starter code used
 """
 
+# Task function (converts and returns)
 def celsius_to_fahrenheit(celsius: float) -> float:
     """
     Converts celsius to fahrenheit
@@ -17,6 +18,7 @@ def celsius_to_fahrenheit(celsius: float) -> float:
     """
     return (celsius * (9/5)) + 32
 
+# Side-effect (Receives input and prints)
 def get_celsius() -> float:
     """
     Forces user to enter valid float (negative supported, no range limit)
@@ -33,6 +35,7 @@ def get_celsius() -> float:
         print("Invalid input")
     return get_celsius()
 
+# Side-effect (Prints to the console)
 def output_conversion(celsius: float, fahrenheit: float) -> None:
     """
     Outputs original input and conversion to output stream
@@ -45,6 +48,7 @@ def output_conversion(celsius: float, fahrenheit: float) -> None:
     """
     print(f"{celsius} degrees Celsius is equal to {fahrenheit} degrees Fahrenheit.")
 
+# Orchestrator (Main is always orchestrator
 def main() -> None:
     """
     Main entry for the program
